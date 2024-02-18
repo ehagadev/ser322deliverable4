5f12617f76ed769c5258aa9eba081a7a720f5a5b
ehagadev <hernandezhagadev@gmail.com>
2024-02-17 23:43:28 -0800

    first commit

---
+++ src/test/java/com/ser322deliverable4/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ser322deliverable4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
