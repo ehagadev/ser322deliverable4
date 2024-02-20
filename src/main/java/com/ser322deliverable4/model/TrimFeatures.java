1d7b2e50e0eeb885721b229404eb206200bcc6f7
David McConnell <mccodav1@gmail.com>
2024-02-20 06:20:56 +0100

    Change TrimFeatures to use composite key (TrimLevel.trimId, feature.featureName)

--- src/main/java/com/ser322deliverable4/model/TrimFeatures.java
+++ src/main/java/com/ser322deliverable4/model/TrimFeatures.java
@@ -1,45 +1,55 @@
 package com.ser322deliverable4.model;
 
+import com.ser322deliverable4.composites.TrimFeatureId;
 import jakarta.persistence.*;
+
+import java.util.Objects;
 
 @Entity
 @Table(name = "trim_features")
 public class TrimFeatures {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @EmbeddedId
+    private TrimFeatureId id;
 
+    @MapsId("trimId")
     @ManyToOne
-    @JoinColumn(name = "feature", referencedColumnName = "name")
-    private Feature feature;
-
-    @ManyToOne
-    @JoinColumn(name = "trim_level", referencedColumnName = "trimId")
+    @JoinColumn(name="trimId", referencedColumnName = "trimId")
     private TrimLevel trimLevel;
+
+    @MapsId("featureName")
+    @ManyToOne
+    @JoinColumn(name="featureName", referencedColumnName = "name")
+    private Feature feature;
 
     public TrimFeatures() {
     }
 
     public TrimFeatures(Feature feature, TrimLevel trimLevel) {
-        this.feature = feature;
         this.trimLevel = trimLevel;
+        this.feature = feature;
+        this.id = new TrimFeatureId(trimLevel.getTrimId(), feature.getName());
     }
 
-    public Long getId() {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TrimFeatures that = (TrimFeatures) o;
+        return Objects.equals(id, that.id) && Objects.equals(trimLevel, that.trimLevel) && Objects.equals(feature, that.feature);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, trimLevel, feature);
+    }
+
+    public TrimFeatureId getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(TrimFeatureId id) {
         this.id = id;
-    }
-
-    public Feature getFeature() {
-        return feature;
-    }
-
-    public void setFeature(Feature feature) {
-        this.feature = feature;
     }
 
     public TrimLevel getTrimLevel() {
@@ -48,5 +58,13 @@ public class TrimFeatures {
 
     public void setTrimLevel(TrimLevel trimLevel) {
         this.trimLevel = trimLevel;
+    }
+
+    public Feature getFeature() {
+        return feature;
+    }
+
+    public void setFeature(Feature feature) {
+        this.feature = feature;
     }
 }
