89896e5519142ea8c3b992b36129a7d89b5a22c5
mswift5 <113479788+mswift5@users.noreply.github.com>
2024-02-20 15:11:35 -0800

    Implements findVehicleByVin for the VehicleRepository.java

--- src/main/java/com/ser322deliverable4/repository/VehicleRepository.java
+++ src/main/java/com/ser322deliverable4/repository/VehicleRepository.java
@@ -1,9 +1,26 @@
 package com.ser322deliverable4.repository;
 
+import com.ser322deliverable4.model.User;
 import com.ser322deliverable4.model.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, String> {
+
+    /**
+     * returns wrapped in Optional in case User is null
+     * @param vin
+     * @return Vehicle
+     */
+    @Query("SELECT v FROM Vehicle v WHERE v.vin = :vin")
+    Optional<Vehicle> findVehicleByVin(@Param("vin") String vin);
+
 }
