048d8ca0bcecee8560d05de19eca1f42beba5576
Johnathan Speck <johnathans96@gmail.com>
2024-02-23 13:59:24 +0100

    webpage loads

--- src/main/java/com/ser322deliverable4/controller/SaveController.java
+++ src/main/java/com/ser322deliverable4/controller/SaveController.java
@@ -1,7 +1,11 @@
 package com.ser322deliverable4.controller;
 
 import com.ser322deliverable4.model.Saves;
+import com.ser322deliverable4.model.User;
+import com.ser322deliverable4.model.Vehicle;
 import com.ser322deliverable4.service.save.ISaveService;
+import com.ser322deliverable4.service.user.IUserService;
+import com.ser322deliverable4.service.vehicle.IVehicleService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -18,11 +22,32 @@ import java.util.List;
 @Controller
 public class SaveController {
 
+    //private final IVehicleService vehicleService;
+
+    //private final IUserService userService;
+
     private final ISaveService saveService;
+
     private final Logger logger = LoggerFactory.getLogger(SaveController.class);
 
     public SaveController(ISaveService saveService) {
         this.saveService = saveService;
     }
+
+    @GetMapping("/saves-services")
+    public String addSavePage(Model model) {
+        //List<User> userList = userService.getAllUsers();
+        //model.addAttribute("userList", userList);
+
+        //List<Vehicle> vehicleList = vehicleSerice.getAllVehicles();
+        //model.addAtrribute("vehicleList", vehicleList);
+
+        //List<Save> saveList = saveService.getAllSaves();
+        //model.attribute("savesList", savesList);
+
+        return "saves-services";
+    }
+
+
 
 }
